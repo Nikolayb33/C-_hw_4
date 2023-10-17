9ed22a5d4bbfafdf7e87556da6f70533fa723d5f
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2023-10-17 11:42:20 +0300

    add task_1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// В решении реализовано возведение числа в степень с помощью модуля Math с выводом результата в console
+// int A = new Random().Next(1,10);
+// int B = new Random().Next(1,10);
+
+// System.Console.WriteLine($"{A}^{B} = {Math.Pow(A,B)}");
+
